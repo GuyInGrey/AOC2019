e6f62670098b303cad6a6d4bd64e8619d141ffc7
GuyInGrey <guyingreydaboss@gmail.com>
2019-12-06 00:18:40 -0700

    Day 6

---
+++ AOC05/AOC05/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AOC02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt").Split(',').ToList().ConvertAll(a => int.Parse(a)).ToArray();
+            ParseIntCode2(input);
+
+            Console.WriteLine("Finished.");
+            Console.Read();
+        }
+
+        static (int, int)[] OpCodeParameters = new (int, int)[]
+        {
+            (1, 3),
+            (2, 3),
+            (3, 1),
+            (4, 1),
+        };
+
+        public static int[] ParseIntCode2(int[] input)
+        {
+            var instructionPointer = 0;
+            while (true)
+            {
+                var current = input[instructionPointer];
+                var currentS = current.ToString("D5");
+                var opcode = current % 100;
+                var modes = currentS.Substring(0, currentS.Length - 2).Select(d => int.Parse(d.ToString())).ToList().ConvertAll(a => a == 1 ? true : false); // true = immediate
+                modes.Reverse();
+
+                switch (opcode)
+                {
+                    case 1: // a * b at c
+                        var a = GetParameter(0);
+                        var b = GetParameter(1);
+                        var c = input[instructionPointer + 3];
+                        input[c] = a + b;
+                        instructionPointer += 4;
+                        break;
+                    case 2:
+                        a = GetParameter(0);
+                        b = GetParameter(1);
+                        c = input[instructionPointer + 3];
+                        input[c] = a * b;
+                        instructionPointer += 4;
+                        break;
+                    case 3:
+                        input[input[instructionPointer + 1]] = int.Parse(Console.ReadLine());
+                        instructionPointer += 2;
+                        break;
+                    case 4:
+                        Console.WriteLine("Output: " + GetParameter(0));
+                        instructionPointer += 2;
+                        break;
+                    case 5:
+                        if (GetParameter(0) != 0)
+                        {
+                            instructionPointer = GetParameter(1);
+                        }
+                        else
+                        {
+                            instructionPointer += 3;
+                        }
+                        break;
+                    case 6:
+                        if (GetParameter(0) == 0)
+                        {
+                            instructionPointer = GetParameter(1);
+                        }
+                        else
+                        {
+                            instructionPointer += 3;
+                        }
+                        break;
+                    case 7:
+                        input[input[instructionPointer + 3]] = GetParameter(0) < GetParameter(1) ? 1 : 0;
+                        instructionPointer++;
+                        break;
+                    case 8:
+                        input[input[instructionPointer + 3]] = GetParameter(0) == GetParameter(1) ? 1 : 0;
+                        instructionPointer++;
+                        break;
+                    case 99:
+                        goto exit;
+                    default:
+                        instructionPointer++;
+                        break;
+                }
+
+                int GetParameter(int i)
+                {
+                    return modes[i] ? input[instructionPointer + i + 1] : input[input[instructionPointer + i + 1]];
+                }
+            }
+
+            exit:;
+            return input;
+        }
+    }
+}
---
+++ AOC06/AOC06/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC06
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+            var lines = input.Split('\n');
+
+            var s = Stopwatch.StartNew();
+            var orbits = lines.ToList().ConvertAll(a => a.Split(')').ToList().ConvertAll(b => b.Trim()));
+
+            var COM = new Node() { ID = "COM", Parent = null };
+            GetOrbits(COM, orbits);
+            s.Stop();
+
+            Console.WriteLine("Enter first ID.");
+            var id1 = Console.ReadLine();
+            Console.WriteLine("Enter second ID.");
+            var id2 = Console.ReadLine();
+
+            s.Start();
+            var one = SearchForNode(COM, id1);
+            var two = SearchForNode(COM, id2);
+            var d = Distance(one, two) - 2;
+            Console.WriteLine("Distance: " + d);
+            Console.WriteLine("Total Direct And Indirect: " + Node.TotalChildren(COM));
+            s.Stop();
+            Console.WriteLine("Took " + s.ElapsedMilliseconds + " milliseconds.");
+            Console.Read();
+        }
+
+        public static Node GetOrbits(Node parent, List<List<string>> orbits)
+        {
+            foreach (var o in orbits)
+            {
+                if (o[0] == parent.ID)
+                {
+                    var n = new Node() { ID = o[1], Parent = parent };
+                    parent.Children.Add(n);
+                    n = GetOrbits(n, orbits);
+                }
+            }
+
+            return parent;
+        }
+
+        public static Node SearchForNode(Node top, string id)
+        {
+
+            if (top.ID == id) { return top; }
+            foreach (var c in top.Children)
+            {
+                var n = SearchForNode(c, id);
+                if (n != null) { return n; }
+            }
+
+            return null;
+        }
+        
+        public static int Distance(Node a, Node b)
+        {
+            if (a == null || b == null) { return -1; }
+            NodesChecked = 0;
+            return Search(0, a, b, new List<Node>());
+        }
+
+        public static int NodesChecked = 0;
+
+        public static int Search(int depth, Node a, Node b, List<Node> Searched)
+        {
+            //Console.WriteLine("Search: a is " + a.ID + "; Relatives are: " + string.Join(",", a.Relatives));
+            NodesChecked++;
+            foreach (var r in a.Relatives)
+            {
+                if (r.ID == b.ID)
+                {
+                    return depth + 1;
+                }
+            }
+
+            Searched.Add(a);
+            foreach (var r in a.Relatives)
+            {
+                if (!Searched.Contains(r))
+                {
+                    var s = Search(depth + 1, r, b, Searched);
+                    if (s != -1)
+                    {
+                        return s;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+
+    public class Node
+    {
+        public string ID;
+        public List<Node> Children = new List<Node>();
+        public Node Parent;
+        public List<Node> Relatives => Children.Concat(new List<Node>() { Parent }).ToList();
+
+        public int StepsToTop()
+        {
+
+            var cnt = 0;
+            var parent = Parent;
+            
+            while (true)
+            {
+                if (parent == null) { goto exit; }
+                cnt++;
+                parent = parent.Parent;
+            }
+
+            exit:;
+            return cnt;
+        }
+
+        public static int TotalChildren(Node n)
+        {
+            var cnt = 0;
+            n.Children.ForEach(ch =>
+            {
+                cnt += ch.StepsToTop();
+                cnt += TotalChildren(ch);
+            });
+            return cnt;
+        }
+
+        public override string ToString()
+        {
+            return ID;
+        }
+    }
+}
