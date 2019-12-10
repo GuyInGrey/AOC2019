a014f0c1d3aaf47fbcc04852e4dafdfd7af26121
GuyInGrey <guyingreydaboss@gmail.com>
2019-12-10 09:29:13 -0700

    Non-functioning Part 2

--- AOC10/AOC10/Program.cs
+++ AOC10/AOC10/Program.cs
@@ -23,7 +23,7 @@ namespace AOC10
                 }
             }
             // Get number of visible asteroids from each asteroid
-            var newAsteroids = new List<(Vector2, int)>(); 
+            var newAsteroids = new List<(Vector2, float)>(); 
             for (var i = 0; i < asteroids.Count; i++)
             {
                 var a1 = asteroids[i];
@@ -39,8 +39,54 @@ namespace AOC10
                 newAsteroids.Add((a1, angles.Distinct().Count()));
             }
             newAsteroids.Sort((a, b) => b.Item2.CompareTo(a.Item2));
+            var bestAsteroid = newAsteroids[0].Item1;
+            asteroids.Remove(bestAsteroid);
+            Console.WriteLine("Best Asteroid: " + bestAsteroid);
 
-            Console.WriteLine(newAsteroids[0].Item2);
+            //Get angles from best asteroid to all others
+            newAsteroids.Clear();
+            for (var i = 0; i < asteroids.Count; i++)
+            {
+                var angle = bestAsteroid.AngleTo(asteroids[i]);
+                newAsteroids.Add((asteroids[i], asteroids[i].Y == bestAsteroid.Y && angle > 6.2f ? 0 : angle));
+            }
+            newAsteroids.Sort((a, b) => b.Item2.CompareTo(a.Item2));
+            newAsteroids.Reverse();
+
+            //Vaporize Asteroids one by one :D BY GIANT LAZERZ
+            var removedOrder = new List<(Vector2, float)>();
+            var lastRemoved = (Vector2.Zero, 0f);
+            while (newAsteroids.Count > 0)
+            {
+                var current = newAsteroids[0];
+                if (current.Item2 != lastRemoved.Item2)
+                {
+                    a:;
+                    Console.WriteLine("Blasted " + current.Item1);
+                    lastRemoved = current;
+                    removedOrder.Add(current);
+                    newAsteroids.RemoveAt(0);
+                }
+                else
+                {
+                    if (newAsteroids[0].Item2 == newAsteroids.Last().Item2)
+                    {
+                        Console.WriteLine("Blasted " + current.Item1);
+                        lastRemoved = current;
+                        removedOrder.Add(current);
+                        newAsteroids.RemoveAt(0);
+                    }
+                    else
+                    {
+                        newAsteroids.RemoveAt(0);
+                        newAsteroids.Add(current);
+                    }
+                }
+            }
+            var last = removedOrder.Last();
+            //var num = (removedOrder[199].Item1.X * 100) + removedOrder[199].Item1.Y;
+            //Console.WriteLine(num);
+
             Console.Read();
         }
     }
@@ -50,10 +96,7 @@ namespace AOC10
         public float X = 0;
         public float Y = 0;
 
-        public Vector2()
-        {
-
-        }
+        public Vector2() { }
 
         public Vector2(float x, float y)
         {
@@ -61,23 +104,20 @@ namespace AOC10
             Y = y;
         }
 
-        public float AngleTo(Vector2 b)
-        {
-            return AngleTo(this, b);
-        }
+        public float AngleTo(Vector2 b) =>
+            AngleTo(this, b);
 
-        public static float AngleTo(Vector2 a, Vector2 b)
-        {
-            return (float)Math.Atan2(b.Y - a.Y, b.X - a.X);
-        }
+        public static float AngleTo(Vector2 a, Vector2 b) =>
+            (float)(Math.Atan2(a.Y - b.Y, b.X - a.X) + Math.PI);
 
-        public override bool Equals(object obj)
-        {
-            if (obj is Vector2 a)
-            {
-                return X == a.X && Y == a.Y;
-            }
-            return false;
-        }
+        public override bool Equals(object obj) =>
+            obj is Vector2 b ? X == b.X && Y == b.Y : false;
+
+        public static Vector2 Zero => new Vector2(0, 0);
+
+        public static Vector2 One => new Vector2(1, 1);
+
+        public override string ToString() =>
+            "(" + X + ", " + Y + ")";
     }
 }
